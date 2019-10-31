493c538170f7c7fababbc32d7ffe444445b74261
Damien Brain <damien.a.brain@gmail.com>
2019-11-01 00:40:32 +1100

    Clean up

--- src/main/java/poc/Converter.java
+++ src/main/java/poc/Converter.java
@@ -1,80 +1,39 @@
 package poc;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.channels.Channels;
-import java.util.List;
-
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.AvroCoder;
 import org.apache.beam.sdk.io.FileIO;
-import org.apache.beam.sdk.io.FileIO.ReadableFile;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.parquet.ParquetIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-
-import poc.config.ConfigBuilder;
-import poc.output.CurationOutputPipelineBuilder;
+import poc.output.AvroToCsv;
 import poc.config.Config;
+import poc.input.CsvReader;
 import poc.util.Avro;
-
-import org.apache.beam.sdk.transforms.DoFn;
 
 public class Converter {
 
 	static void runConverter(PipelineOptions options) {
 		Pipeline p = Pipeline.create(options);
-		Config config = ConfigBuilder.read("config.json");
-		String inputSchema = config.getInputSchemaJson();
+		String inputSchema = Config.getInputSchemaJson();
 		
-		p.apply(FileIO.match().filepattern("multilineinput.csv"))
-			.apply(FileIO.readMatches())
-			.apply(ParDo.of(new DoFn<ReadableFile, GenericRecord>() {
-				@ProcessElement
-				public void process(ProcessContext c) throws IOException {
-					try(InputStream is = Channels.newInputStream(c.element().open())) {
-						try {
-							CSVReader csvr = new CSVReader(new InputStreamReader(is));
-							List<String[]> rows = csvr.readAll();
-							csvr.close();
+		PCollection<GenericRecord> records =
+			p.apply(FileIO.match().filepattern("multilineinput.csv"))
+				.apply(FileIO.readMatches())
+				.apply(ParDo.of(new CsvReader()))
+				.setCoder(AvroCoder.of(GenericRecord.class, Avro.fromJson(inputSchema)));
 
-							Schema schema = new Schema.Parser().parse(inputSchema);
-
-							for(String[] row : rows) {
-								// Create Avro Generic Record
-								GenericRecord genericRecord = new GenericData.Record(schema);
-								List<Schema.Field> fields = schema.getFields();
-								for (int index = 0; index < fields.size(); ++index) {
-									Schema.Field field = fields.get(index);
-									genericRecord.put(field.name(), row[index]);
-								}
-								/*
-								for(String s : row) {
-									System.out.println("<<<<<");
-									System.out.println(s);
-									System.out.println(">>>>>");
-								}
-								*/
-								c.output(genericRecord);
-							}
-						}
-						catch(CsvException e) {
-							System.out.println("CSVException: " + e.toString());
-						}
-					}
-				}
-			}
-			))
-			.setCoder(AvroCoder.of(GenericRecord.class, Avro.fromJson(inputSchema)))
-			.apply(new CurationOutputPipelineBuilder(config));
-			;
+		records
+			.apply(ParDo.of(new AvroToCsv()))
+			.apply("Writelines", TextIO.write().to("testoutput.txt"));
+			
+		records
+			.apply(FileIO.<GenericRecord>write().via(ParquetIO.sink(Avro.fromJson(inputSchema))).to("output.parquet"));
 	
 		p.run().waitUntilFinish();
 	}
--- src/main/java/poc/config/Config.java
+++ src/main/java/poc/config/Config.java
@@ -3,38 +3,15 @@ package poc.config;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class Config implements java.io.Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1239013818939321647L;
-	private Map<String, Field> fields;
-	private String inputSchemaFile;
+	private static final String inputSchemaFile = "input.schema";
 
-	public Config() {
-		fields = new LinkedHashMap<String, Field>();
-	}
-
-	public Map<String, Field> getFields() {
-		return fields;
-	}
-
-	public void setFields(Map<String, Field> fields) {
-		this.fields = fields;
-	}
-
-	public String getInputSchemaFile() {
-		return inputSchemaFile;
-	}
-
-	public void setInputSchemaFile(String inputSchemaFile) {
-		this.inputSchemaFile = inputSchemaFile;
-	}
-
-	public String getInputSchemaJson() {
+	public static String getInputSchemaJson() {
 		try {
 			return(readJson(inputSchemaFile));
 		}
@@ -46,18 +23,5 @@ public class Config implements java.io.Serializable {
 
 	private static String readJson(String schemaPath) throws IOException {
 		return new String(Files.readAllBytes(Paths.get(schemaPath)));
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder output = new StringBuilder();
-
-		for(Map.Entry<String, Field> field: fields.entrySet()) {
-			output.append(field.getKey() + ": " + field.getValue().toString());
-			output.append("\n");
-		}
-		
-		output.append("inputSchemaFile: " + getInputSchemaFile() + "\n");
-		return output.toString();
 	}
 }
